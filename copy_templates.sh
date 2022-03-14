bbb68650eacc88d8e02ebeda54b14b1df31af6f3
e. alvarez <mail@alvar3z.com>
2022-03-14 00:37:34 -0400

    script refactored

--- copy_templates.sh
+++ copy_templates.sh
@@ -1,5 +1,7 @@
+#!/usr/bin/bash
+
 cd /tmp
-git clone https://github.com/ryangrose/easy-pandoc-templates.git
+git clone https://github.com/ealvar3z/easy-pandoc-templates.git
 cd easy-pandoc-templates/html
 mkdir -p ~/.pandoc/templates
 for file in ./*.html; do
